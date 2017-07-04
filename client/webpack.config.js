58d5c20f374bc5d691d36ab8834d73051af2d26a
vs-zhang <vincentzhang1209@gmail.com>
2017-07-03 23:05:56 -0400

    make front-end dockernize

---
+++ client/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: ['./src/main.js'],
+  devServer: {
+    hot: true,
+    inline: true,
+    host: '0.0.0.0',
+    publicPath: '/',
+    historyApiFallback: true,
+    port: 5002,
+    contentBase: './src',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/
+        ],
+        loader: 'babel-loader',
+        query: {
+          presets: ["es2015", "stage-0", "react"]
+        }
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: 'url-loader?limit=8192&name=images/[name]-[hash:6].[ext]'
+      },
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      chunkSortMode: 'dependency',
+      filename: 'index.html',
+      hash: false,
+      inject: 'body',
+      template: './src/index.tpl.html'
+    })
+  ],
+  bail: true
+};
