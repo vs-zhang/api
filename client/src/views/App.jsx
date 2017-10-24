b114c441a077c3bd298236ca057c8e3ca28884cc
vs-zhang <vincentzhang1209@gmail.com>
2017-10-23 23:02:18 -0400

    reformat code

--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -1,18 +1,62 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ui from 'redux-ui';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import Cookies from 'universal-cookie';
+import { ConnectedRouter } from 'react-router-redux';
+import jwtDecode from 'jwt-decode';
 import 'semantic-ui-css/semantic.min.css';
 import MainRouter from './routers';
+import { isAuthenticated, authActions } from '../core/auth';
+import { Navbar } from './Components';
 import './styles/app.css';
 
-export default class App extends Component {
+class App extends Component {
     static propTypes = {
+        initLoggedIn: PropTypes.func.isRequired,
+        reIssueAccessToken: PropTypes.func.isRequired,
+        isAuth: PropTypes.bool.isRequired,
         history: PropTypes.object.isRequired
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            cookies: new Cookies(),
+        };
+    }
+
+    componentWillMount() {
+        const accessToken = this.state.cookies.get('access_token');
+        if (accessToken) {
+            const { exp, sub: user } = jwtDecode(accessToken);
+            console.log(exp * 1000);
+            console.log(Date.now());
+            console.log(user);
+            if (exp * 1000 < Date.now()) {
+                console.log('re issue access token');
+                this.props.reIssueAccessToken();
+            } else {
+                console.log('valid access token');
+                this.props.initLoggedIn(user);
+            }
+        }
+    }
+
     render() {
-        const { history } = this.props;
+        const { isAuth, history } = this.props;
 
         const mainApp = (
-            <MainRouter history={ history } />
+            <ConnectedRouter history={ history }>
+                <div>
+                    <Navbar isAuth={ isAuth } />
+                    <div>
+                        <MainRouter history={ history } />
+                    </div>
+                </div>
+            </ConnectedRouter>
         );
 
         return (
@@ -22,3 +66,15 @@ export default class App extends Component {
         );
     }
 }
+
+const mapStateToProps = createSelector(
+  isAuthenticated,
+  isAuth => ({ isAuth })
+);
+
+const uiState = {
+    isSidebarOpen: false
+};
+
+const connectComponent = connect(mapStateToProps, authActions)(App);
+export default ui({ key: 'home', state: uiState })(connectComponent);
--- client/src/views/routers/routers.jsx
+++ client/src/views/routers/routers.jsx
@@ -3,71 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import { Route, Switch } from 'react-router';
-import { ConnectedRouter } from 'react-router-redux';
-import Cookies from 'universal-cookie';
-import jwtDecode from 'jwt-decode';
-import ui from 'redux-ui';
 import { HomePage, LoginPage, SignupPage, SettingsPage } from '../Pages';
 import AuthRoute from './AuthRoute';
 import PrivateRoute from './PrivateRoute';
 import { isAuthenticated, authActions } from '../../core/auth';
-import { Navbar } from '../Components';
 
 class MainRouter extends Component {
     static propTypes = {
-        initLoggedIn: PropTypes.func.isRequired,
-        reIssueAccessToken: PropTypes.func.isRequired,
         isAuth: PropTypes.bool.isRequired,
-        history: PropTypes.object.isRequired
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            cookies: new Cookies(),
-        };
-    }
-
-    componentWillMount() {
-        const accessToken = this.state.cookies.get('access_token');
-        if (accessToken) {
-            const { exp, sub: user } = jwtDecode(accessToken);
-            console.log(exp * 1000);
-            console.log(Date.now());
-            console.log(user);
-            if (exp * 1000 < Date.now()) {
-                console.log('re issue access token');
-                this.props.reIssueAccessToken();
-            } else {
-                console.log('valid access token');
-                this.props.initLoggedIn(user);
-            }
-        }
-
     }
 
     render() {
-        const { isAuth, history } = this.props;
-        const routes = (
+        const { isAuth } = this.props;
+
+        return (
             <Switch>
                 <AuthRoute path="/signup" component={ SignupPage } isAuthenticated={ isAuth } />
                 <AuthRoute path="/login" component={ LoginPage } isAuthenticated={ isAuth } />
-                <PrivateRoute path="/settings" component={ SettingsPage } isAuthenticated={ isAuth || !!this.state.cookies.get('refresh_token') } />
+                <PrivateRoute path="/settings" component={ SettingsPage } isAuthenticated={ isAuth } />
                 <Route exact path="/home" component={ HomePage } />
                 <Route exact path="/" component={ HomePage } />
             </Switch>
-        );
-
-        return (
-            <ConnectedRouter history={ history }>
-                <div>
-                    <Navbar isAuth={ isAuth } />
-                    <div>
-                        {routes}
-                    </div>
-                </div>
-            </ConnectedRouter>
         );
     }
 }
@@ -77,9 +33,4 @@ const mapStateToProps = createSelector(
   isAuth => ({ isAuth })
 );
 
-const uiState = {
-    isSidebarOpen: false
-};
-
-const connectComponent = connect(mapStateToProps, authActions)(MainRouter);
-export default ui({ key: 'home', state: uiState })(connectComponent);
+export default connect(mapStateToProps, authActions)(MainRouter);
