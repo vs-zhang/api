9d5b51e710c33d1c6ea26c3afdb0d14233eabf25
vs-zhang <vincentzhang1209@gmail.com>
2017-11-19 00:35:30 -0500

    some update

--- api/app/application.py
+++ api/app/application.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
 from flasgger import Swagger
 from flask_cors import CORS, cross_origin
-from app.auth.controller import auth
+from dotenv import load_dotenv
+from auth.controller import auth
+
+# load dotenv in the base root
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env.local')
+load_dotenv(dotenv_path)
 
 application = Flask(__name__)
 cors = CORS(application, resources={r"/*": {"origins": "*", "supports_credentials": True}})
